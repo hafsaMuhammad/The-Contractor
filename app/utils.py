52f2dd844b5a29f8692d5fe7c4616e46202fa595
hafsa_mohamed <hafsa.mohammed@t3.com.eg>
2025-10-03 15:03:56 +0300

    edit email

--- app/utils.py
+++ app/utils.py
@@ -4,8 +4,8 @@ from django.template.loader import render_to_string
 def send_order_email(order):
     subject = f"New Order #{order.id}"
     from_email = "TheContractor@test-xkjn41m6qv64z781.mlsender.net"
-    to_email = ["m.mohamedshadad@gmail.com"]
-
+    to_email = ["hafsa.mohamed1999@gmail.com"]
+    
     html_content = render_to_string("emails/order_created.html", {"order": order})
 
     email = EmailMultiAlternatives(subject, "", from_email, to_email)
